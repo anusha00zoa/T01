f3d76f8899df27aa0e6322f2f9cbf43e39b9047a
anusha00zoa <anusha00zoa@gmail.com>
2016-09-13 11:27:41 -0500

    Storyboard rough design to team01

---
+++ T01/LevelTwoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LevelTwoViewController.swift
+//  T01
+//
+//  Created by SINGH ANUSHA  on 9/12/16.
+//  Copyright © 2016 SINGH ANUSHA . All rights reserved.
+//
+
+import UIKit
+
+class LevelTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
